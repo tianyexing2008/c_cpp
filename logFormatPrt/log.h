d5a72a238e2cf021ba06b346a9718af7b9b20527
liangshen <liangshen@uni-ubi.com>
2021-05-19 17:26:54 +0800

    宏定义添加空格，否则c++11编译错误

--- logFormatPrt/log.h
+++ logFormatPrt/log.h
@@ -15,7 +15,7 @@ static int s_color[] = {0, COLOR_INFO, COLOR_WARN, COLOR_ERROR, COLOR_FATAL};
 
 #define PRINT(title, level, file, func, line, fmt, ...) \
     printf("\033[%d;40m", s_color[level]); \
-    printf("[%s]tid:%ld, file: %s, func: %s, line: %d, "fmt"\033[0m", title, syscall(SYS_gettid), file, func, line, ##__VA_ARGS__)
+    printf("[%s]tid:%ld, file: %s, func: %s, line: %d, " fmt"\033[0m", title, syscall(SYS_gettid), file, func, line, ##__VA_ARGS__)
 
 #define PRINT_INFO(title, level, fmt, ...) \
     PRINT(title, level, __FILE__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
