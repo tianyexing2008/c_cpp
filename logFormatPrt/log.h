5fb07bcea980cee562b80a4c5a55b30670079e9e
tianyexing2008 <52118912+tianyexing2008@users.noreply.github.com>
2022-08-29 13:23:43 +0800

    Update log.h
    add timestamp

--- logFormatPrt/log.h
+++ logFormatPrt/log.h
@@ -3,6 +3,23 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
+static std::string getTimeStamp()
+{
+    time_t now;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    now = tv.tv_sec;
+    struct tm localTime;
+    localtime_r(&now, &localTime);
+    
+    char timeBuf[128] = {0};
+    char outBuf[128] = {0};
+    strftime((char *)timeBuf, sizeof(timeBuf), "%Y/%m/%d %H:%M:%S", &localTime);
+    snprintf(outBuf, sizeof(outBuf), "%s %06u", timeBuf, tv.tv_usec % 1000000);
+
+    return std::string(outBuf);
+}
+
 enum COLOR
 {
     COLOR_INFO = 32,
@@ -15,13 +32,14 @@ static int s_color[] = {0, COLOR_INFO, COLOR_WARN, COLOR_ERROR, COLOR_FATAL};
 
 #define PRINT(title, level, file, func, line, fmt, ...) \
     printf("\033[%d;40m", s_color[level]); \
-    printf("[%s]tid:%ld, file: %s, func: %s, line: %d, " fmt"\033[0m", title, syscall(SYS_gettid), file, func, line, ##__VA_ARGS__)
+    printf("[%s %s]tid:%ld, file: %s, func: %s, line: %d, " fmt"\033[0m", title, getTimeStamp().c_str(), syscall(SYS_gettid), file, func, line, ##__VA_ARGS__)
 
 #define PRINT_INFO(title, level, fmt, ...) \
     PRINT(title, level, __FILE__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
 
 //打印颜色，等级分为1，2，3，4
-#define tracef(fmt, ...) PRINT_INFO("info", 1, fmt, ##__VA_ARGS__)
+#define tracef(fmt, ...) PRINT_INFO("trace", 0, fmt, ##__VA_ARGS__)
+#define infof(fmt, ...) PRINT_INFO("info", 1, fmt, ##__VA_ARGS__)
 #define warnf(fmt, ...) PRINT_INFO("warn", 2, fmt, ##__VA_ARGS__)
 #define errorf(fmt, ...) PRINT_INFO("error", 3, fmt, ##__VA_ARGS__)
 #define fatalf(fmt, ...) PRINT_INFO("fatal", 4, fmt, ##__VA_ARGS__)
